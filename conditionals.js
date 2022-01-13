4844970814b76a8fbe6964c9714f30845404d96e
Brendan-funk <brendanzfunk@gmail.com>
2022-01-13 11:11:49 +0000

    added logical operator statements to conditionals

--- conditionals.js
+++ conditionals.js
@@ -5,6 +5,9 @@ const temperature = -2;
 if(raining) {
   console.log("Don\'t forget your umbrella!");
 }
+if(!raining){
+  console.log("Leave your umbrella at home!");
+}
 
 if(temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -12,8 +15,21 @@ if(temperature < 0) {
 else if(temperature < 15){
   console.log("Short sleeves won't cut it!");
 }
+else if(temperature < -40 || temperature > 40){
+  console.log("Maybe going outside isn't such a great idea");
+}
 else{
   console.log("Short sleeves are fine.");
 }
 
 console.log("Now you're ready to go outside");
+//Weather example
+
+const isCitizen = true;
+const age = 26;
+
+if(isCitizen && age >18){
+  console.log("You are eligible to vote");
+}
+
+//Voting example
