bb70d5800362a0d0b9e154bbb90643d6176e32ee
Brendan-funk <brendanzfunk@gmail.com>
2022-01-15 02:03:21 +0000

    added rangeLoop.js

---
+++ rangeLoop.js
@@ -0,0 +1,22 @@
+const range = function(start,end,step){
+  
+  //checking if the inputs are valid, if not return empty array
+  if(start < 0 || start === null || end === null || step === null || end < start){
+    return [];
+  }
+  else{
+
+    //defining loop variable and array to return
+    let arr = [];
+    let i = start;
+
+    //looping through the count adding the number to the array each time
+    while(i < end){
+      arr.push(i);
+      i += step;
+    }
+
+    //return array
+    return arr;
+  }
+}
